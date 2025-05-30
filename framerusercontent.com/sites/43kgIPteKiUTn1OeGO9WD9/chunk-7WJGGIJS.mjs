5b672f997efa0ca622dc47f0a13df351f5c55b61
GitHub Action <action@github.com>
2025-05-30 05:43:08 -0400

    Deploy static Framer site

---
+++ framerusercontent.com/sites/43kgIPteKiUTn1OeGO9WD9/chunk-7WJGGIJS.mjs
@@ -0,0 +1,21 @@
+function t(e,a){return{customHTMLHeadStart:`<!-- Primary Meta Tags -->
+<title>Landify - One page templates</title>
+<meta name="title" content="Landify - One page templates">
+<meta name="description" content="Landify is a versatile one-page template, designed to cater to diverse business needs. Currently in its version 1.0, it offers 5 distinct designs, each crafted with attention to detail and functionality.">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.framer.com/marketplace/template/landify/">
+<meta property="og:title" content="Landify - One page templates">
+<meta property="og:description" content="Landify is a versatile one-page template, designed to cater to diverse business needs. Currently in its version 1.0, it offers 5 distinct designs, each crafted with attention to detail and functionality.">
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://www.framer.com/marketplace/template/landify/">
+<meta property="twitter:title" content="Landify - One page templates">
+<meta property="twitter:description" content="Landify is a versatile one-page template, designed to cater to diverse business needs. Currently in its version 1.0, it offers 5 distinct designs, each crafted with attention to detail and functionality.">
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png">
+
+<!-- Meta Tags Generated with https://metatags.io -->`,description:"Simplify your online presence with our versatile one-page template.",favicon:"https://framerusercontent.com/assets/meNQRP8Y6HwvSZAztoIflCNXQI.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/jQSabXwKFVXS1laPDeEosAJIzHc.jpg",title:"Designor- one page template"}}export{t as a};
+//# sourceMappingURL=chunk-7WJGGIJS.mjs.map
